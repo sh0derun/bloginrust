4b1330d7e3f60bc44bfba77c2bde55e86d08d785
Sh@drun <razik.anas@gmail.com>
2020-11-30 04:16:21 +0100

    add response struct + some Methods/StatusCode constants

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,85 @@
-use std::net::{TcpListener};
-use std::io::{Write, Read};
+use std::io::{Read, Write};
+use std::net::TcpListener;
 
-struct Request{
+struct Method;
+impl Method{
+    const GET: &'static str = "GET";
+    const POST: &'static str = "POST";
+    const PUT: &'static str = "PUT";
+    const DELETE: &'static str = "DELETE";
+    const HEAD: &'static str = "HEAD";
+    const CONNECT: &'static str = "CONNECT";
+    const TRACE: &'static str = "TRACE";
+}
+
+struct StatusCode;
+impl StatusCode{
+    const OK: (u16, &'static str)                    = (200, "OK");
+    const NOT_FOUND: (u16, &'static str)             = (404, "Not Found");
+    const NOT_ALLOWED: (u16, &'static str)           = (405, "Method Not Allowed");
+    const INTERNAL_SERVER_ERROR: (u16, &'static str) = (500, "Internal Server Error");
+}
+
+#[derive(Debug)]
+struct Request {
     method: String,
     resource: String,
-    http_version: String
+    http_version: String,
 }
 
-impl Request{
-    fn new()-> Request{
-        Request{
+impl Request {
+    fn new() -> Request {
+        Request {
             method: String::new(),
             resource: String::new(),
-            http_version: String::new()
+            http_version: String::new(),
         }
     }
+
+    fn parse_request(request: String) -> Request {
+        let headers: Vec<&str> = request.split("\n").collect();
+        let request_parts: Vec<&str> = headers[0].split(" ").collect();
+        return Request {
+            method: request_parts[0].to_string(),
+            resource: request_parts[1].to_string(),
+            http_version: request_parts[2].to_string(),
+        };
+    }
+
+    fn handle_request(&self) {
+        //TODO: better way ?
+        match self.method.as_ref() {
+            Method::GET => {
+                println!("this is a GET method !");
+                println!("{:?}", self);
+            }
+            Method::POST => println!("unimplemented POST method !"),
+            Method::PUT => println!("unimplemented PUT method !"),
+            Method::DELETE => println!("unimplemented DELETE method !"),
+            Method::CONNECT => println!("unimplemented CONNECT method !"),
+            Method::TRACE => println!("unimplemented TRACE method !"),
+            Method::HEAD => println!("unimplemented HEAD method !"),
+            _ => println!("out of methods !"),
+        }
+    }
+
 }
 
-fn parse_request(request: String)->Request{
-    let headers:Vec<&str> = request.split("\n").collect();
-    let request_parts:Vec<&str> = headers[0].split(" ").collect();
-    return Request{method      :request_parts[0].to_string(), 
-                   resource    :request_parts[1].to_string(), 
-                   http_version:request_parts[2].to_string()};
+#[derive(Debug)]
+struct Response{
+    http_version: String,
+    status_code: u16,
+    status_name: String,
+    content_type: String,
+    content: String
 }
 
-fn handle_request(request: Request){
-    match request.method.as_ref() {
-        "GET" => println!("this is a GET method !"),
-        "POST" => println!("unimplemented POST method !"),
-        "PUT" => println!("unimplemented PUT method !"),
-        "DELETE" => println!("unimplemented DELETE method !"),
-        "CONNECT" => println!("unimplemented CONNECT method !"),
-        "TRACE" => println!("unimplemented TRACE method !"),
-        "HEAD" => println!("unimplemented HEAD method !"),
-        _ => println!("out of methods !"),
+impl Response{
+    fn new() {
+        //TODO
+    }
+    fn build_response(){
+        //TODO
     }
 }
 
@@ -58,9 +103,9 @@ fn main() {
                 let mut req_buf = [0u8; 1024 * 4];
                 match client.read(&mut req_buf) {
                     Ok(_) => {
-                        let request: Request = parse_request(String::from_utf8_lossy(&req_buf).to_string());
-                        handle_request(request);
-                    },
+                        let request: Request = Request::parse_request(String::from_utf8_lossy(&req_buf).to_string());
+                        request.handle_request();
+                    }
                     Err(e) => println!("Failed receiving request: {}", e),
                 }
 
