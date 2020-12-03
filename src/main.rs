318b6786e85808bf5bce9e095f628ea17753b3e1
Sh@drun <razik.anas@gmail.com>
2020-12-03 05:21:56 +0100

    add struct for content types for later usage. implementing Default and Display for Response struct

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
-use std::fs::{File};
+use std::fs::File;
+use std::fmt::Formatter;
+
+const KBYTE: u16 = 1024;
+const MBYTE: u32 = KBYTE as u32*1000;
 
 struct Method;
 impl Method{
-    const GET: &'static str = "GET";
-    const POST: &'static str = "POST";
-    const PUT: &'static str = "PUT";
-    const DELETE: &'static str = "DELETE";
-    const HEAD: &'static str = "HEAD";
+    const GET: &'static str     = "GET";
+    const POST: &'static str    = "POST";
+    const PUT: &'static str     = "PUT";
+    const DELETE: &'static str  = "DELETE";
+    const HEAD: &'static str    = "HEAD";
     const CONNECT: &'static str = "CONNECT";
-    const TRACE: &'static str = "TRACE";
+    const TRACE: &'static str   = "TRACE";
 }
 
-struct StatusCode;
-impl StatusCode{
+#[derive(Debug)]
+struct Status;
+impl Status{
     const OK: (u16, &'static str)                    = (200, "OK");
     const NOT_FOUND: (u16, &'static str)             = (404, "Not Found");
     const NOT_ALLOWED: (u16, &'static str)           = (405, "Method Not Allowed");
     const INTERNAL_SERVER_ERROR: (u16, &'static str) = (500, "Internal Server Error");
+}
+
+#[derive(Debug)]
+struct ContentType;
+impl ContentType{
+    const HTML: &'static str  = "text/html";
+    const PLAIN: &'static str = "text/plain";
+    const JPEG: &'static str  = "image/jpeg";
+    const PNG: &'static str   = "image/png";
+    const json: &'static str  = "application/json";
 }
 
 #[derive(Debug)]
@@ -47,20 +62,24 @@ impl Request {
         };
     }
 
-    fn handle_request(&self, mut responce: &Response) {
+    fn handle_request(&self, response: &mut Response) {
         //TODO: better way ?
         match self.method.as_ref() {
             Method::GET => {
                 println!("this is a GET method !");
                 println!("{:?}", self);
-            }
-            Method::POST => println!("unimplemented POST method !"),
-            Method::PUT => println!("unimplemented PUT method !"),
-            Method::DELETE => println!("unimplemented DELETE method !"),
-            Method::CONNECT => println!("unimplemented CONNECT method !"),
-            Method::TRACE => println!("unimplemented TRACE method !"),
-            Method::HEAD => println!("unimplemented HEAD method !"),
-            _ => println!("out of methods !"),
+                response.content_type = ContentType::HTML.to_string();
+                response.content = Response::load_content("./src/public/index.html");
+                response.status = Status::OK;
+                response.error = String::new();
+            },
+            Method::POST => {println!("unimplemented POST method !");}
+            Method::PUT => {println!("unimplemented PUT method !");}
+            Method::DELETE => {println!("unimplemented DELETE method !");}
+            Method::CONNECT => {println!("unimplemented CONNECT method !");}
+            Method::TRACE => {println!("unimplemented TRACE method !");}
+            Method::HEAD => {println!("unimplemented HEAD method !");}
+            _ => {println!("out of methods !");}
         }
     }
 
@@ -69,25 +88,42 @@ impl Request {
 #[derive(Debug)]
 struct Response{
     http_version: String,
-    status_code: u16,
-    status_name: String,
+    status: (u16, &'static str),
     content_type: String,
-    content: String
+    content: String,
+    error: String
 }
 
 impl Response{
     fn new() {
         //TODO
     }
-    fn build_response(){
-        //TODO
-    }
 
     fn load_content(file_path: &str) -> String{
+        //TODO : we don't want to panic if there is a problem
         let mut file = File::open(file_path).expect(&format!("cannot open file {}",file_path));
         let mut content = String::new();
         file.read_to_string(&mut content).expect("cannot read from the file !");
         return content;
+    }
+}
+
+impl Default for Response {
+    fn default() -> Self {
+        Response{
+            http_version: "HTTP/1.1".to_string(),
+            content_type: ContentType::PLAIN.to_string(),
+            content:      String::from("dummy response"),
+            status:       Status::OK,
+            error:        String::new()
+        }
+    }
+}
+
+
+impl std::fmt::Display for Response {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result{
+        write!(f, "{} {} {}\nContent-Type: {}\n{}", self.http_version, self.status.0, self.status.1, self.content_type, self.content)
     }
 }
 
@@ -98,36 +134,21 @@ fn main() {
         match server_lstnr.accept() {
             Ok((mut client, address)) => {
                 println!("new client :{}", address);
-                let res = "HTTP/1.1 200 OK
-                                Content-Type: text/html\n
-                                <html>
-                                    <head>
-                                        <title>hello world 5</title>
-                                    </head>
-                                    <body>
-                                        <script>for(let i = 0; i < 5; ++i){document.writeln('<h1>hello world!</h1>');}</script>
-                                    </body>
-                                </html>";
 
-                let mut req_buf = [0u8; 1024 * 4];
-                
-                let response = Response{
-                    http_version: "HTTP/1.1".to_string(),
-                    content_type: "text/html".to_string(),
-                    content:      Response::load_content("./public/index.html"),
-                    status_code: StatusCode::OK.0,
-                    status_name: StatusCode::OK.1.to_string()
-                };
+                //TODO : better way to read the request
+                let mut req_buf = [0u8; KBYTE as usize * 4];
+                let mut response: Response = Default::default();
                 
                 match client.read(&mut req_buf) {
                     Ok(_) => {
+                        println!("{}", String::from_utf8_lossy(&req_buf).to_string());
                         let request: Request = Request::parse_request(String::from_utf8_lossy(&req_buf).to_string());
-                        request.handle_request(&response);
+                        request.handle_request(&mut response);
                     }
                     Err(e) => println!("Failed receiving request: {}", e),
                 }
 
-                match client.write(res.as_bytes()) {
+                match client.write(response.content.as_bytes()) {
                     Ok(_) => println!("Response sent"),
                     Err(e) => println!("Failed sending response: {}", e),
                 }
